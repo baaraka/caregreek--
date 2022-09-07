4a48a9c648056021fb0472d9562806bb66461754
barakaganai <barakaganai01@gmail.com>
2022-09-07 14:10:04 +0300

    create Home.jsx

---
+++ src/home/Home.jsx
@@ -0,0 +1,30 @@
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <div className="homeImg">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgyHJf5HSeHgJxlDkNidBzEp5_PNY3wBAO1w&usqp=CAU"
+          alt=""
+          className="homeImgItem"
+        />
+      </div>
+      <div className="homeTitle">
+        <h1 className="homeTitleItem">About Us</h1>
+      </div>
+      <div className="homeList">
+        <ul className="homeListItem">
+          <li>............</li>
+          <li>............</li>
+          <li>............</li>
+          <li>............</li>
+          <li>............</li>
+          <li>............</li>
+          <li>............</li>
+          <li>............</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
