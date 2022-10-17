f1eb9a8531707558883bdc0a586db8cdea36d704
barakaganai <barakaganai01@gmail.com>
2022-10-17 11:09:07 +0300

    update paragraph

--- src/services/Services.jsx
+++ src/services/Services.jsx
@@ -15,11 +15,10 @@ export default function Services() {
             <div className="containerHeading">
               <h2 className="headings">Residential Cleaning</h2>
               <p className="paragraph">
-                I am a full-stack web developer with strong knowledge of
-                MongoDB, node.js, Javascript and front end technologies such
-                react, jquery, Ejs and counting.Graduated University with degree
-                in science of information technology. Strong mathematics and
-                programming background.
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam,
+                ea amet? Natus sed alias quae similique fugit adipisci obcaecati
+                ex quod voluptatibus ducimus reiciendis laudantium, quia
+                dignissimos, veniam, iusto temporibus!
               </p>
             </div>
           </div>
@@ -28,13 +27,12 @@ export default function Services() {
               <i className="icon fa-solid fa-pump-medical"></i>
             </div>
             <div className="containerHeading">
-              <h2 className="headings">Residential Cleaning</h2>
+              <h2 className="headings">Commercial Cleaning</h2>
               <p className="paragraph">
-                I am a full-stack web developer with strong knowledge of
-                MongoDB, node.js, Javascript and front end technologies such
-                react, jquery, Ejs and counting.Graduated University with degree
-                in science of information technology. Strong mathematics and
-                programming background.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Doloremque assumenda deleniti nulla vero mollitia explicabo
+                aliquam aliquid corrupti esse, hic tenetur illo dolores dicta?
+                Inventore similique quisquam laboriosam obcaecati! Ducimus?
               </p>
             </div>
           </div>
@@ -43,13 +41,12 @@ export default function Services() {
               <i className="icon fa-solid fa-soap"></i>
             </div>
             <div className="containerHeading">
-              <h2 className="headings">Residential Cleaning</h2>
+              <h2 className="headings">Post Construction Cleaning</h2>
               <p className="paragraph">
-                I am a full-stack web developer with strong knowledge of
-                MongoDB, node.js, Javascript and front end technologies such
-                react, jquery, Ejs and counting.Graduated University with degree
-                in science of information technology. Strong mathematics and
-                programming background.
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore
+                non similique quas praesentium? Facilis quae fugit rem. Magnam
+                ex ut in, eos dicta, minus quibusdam alias, et molestiae
+                distinctio odit!
               </p>
             </div>
           </div>
@@ -58,13 +55,13 @@ export default function Services() {
               <i className="icon fa-solid fa-broom"></i>
             </div>
             <div className="containerHeading">
-              <h2 className="headings">Residential Cleaning</h2>
+              <h2 className="headings">Fumigation/Pest Control</h2>
               <p className="paragraph">
-                I am a full-stack web developer with strong knowledge of
-                MongoDB, node.js, Javascript and front end technologies such
-                react, jquery, Ejs and counting.Graduated University with degree
-                in science of information technology. Strong mathematics and
-                programming background.
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae
+                aliquid distinctio explicabo voluptatibus error dolore
+                repellendus. Aliquam voluptatum nobis obcaecati ipsa reiciendis?
+                Perspiciatis incidunt doloremque iste, beatae est excepturi
+                quis.
               </p>
             </div>
           </div>
@@ -73,13 +70,12 @@ export default function Services() {
               <i className="icon fa-solid fa-snowplow"></i>
             </div>
             <div className="containerHeading">
-              <h2 className="headings">Residential Cleaning</h2>
+              <h2 className="headings">Carpet And Upholstering Cleaning</h2>
               <p className="paragraph">
-                I am a full-stack web developer with strong knowledge of
-                MongoDB, node.js, Javascript and front end technologies such
-                react, jquery, Ejs and counting.Graduated University with degree
-                in science of information technology. Strong mathematics and
-                programming background.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad
+                itaque perferendis animi, ipsum perspiciatis minus quae quidem
+                vel, error veritatis quasi dicta fuga delectus, aperiam ex ipsam
+                voluptate nulla laborum.
               </p>
             </div>
           </div>
@@ -88,13 +84,12 @@ export default function Services() {
               <i className="icon fa-solid fa-pump-soap"></i>
             </div>
             <div className="containerHeading">
-              <h2 className="headings">Residential Cleaning</h2>
+              <h2 className="headings">End Of Tenancy Cleaning</h2>
               <p className="paragraph">
-                I am a full-stack web developer with strong knowledge of
-                MongoDB, node.js, Javascript and front end technologies such
-                react, jquery, Ejs and counting.Graduated University with degree
-                in science of information technology. Strong mathematics and
-                programming background.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi,
+                facere fugiat id impedit voluptates commodi explicabo amet
+                facilis aliquam minima eum dolorum, mollitia quae rem veniam
+                doloremque quo nesciunt ut.
               </p>
             </div>
           </div>
@@ -104,13 +99,12 @@ export default function Services() {
               <i className="icon fa-solid fa-pump-soap"></i>
             </div>
             <div className="containerHeading">
-              <h2 className="headings">Residential Cleaning</h2>
+              <h2 className="headings">Site Clearing/Grass Cutting</h2>
               <p className="paragraph">
-                I am a full-stack web developer with strong knowledge of
-                MongoDB, node.js, Javascript and front end technologies such
-                react, jquery, Ejs and counting.Graduated University with degree
-                in science of information technology. Strong mathematics and
-                programming background.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Cupiditate saepe perferendis mollitia velit maxime reiciendis
+                modi vero dicta. Praesentium officiis itaque eligendi est
+                adipisci repellendus sunt incidunt at odio possimus?
               </p>
             </div>
           </div>
@@ -119,13 +113,12 @@ export default function Services() {
               <i className="icon fa-solid fa-pump-soap"></i>
             </div>
             <div className="containerHeading">
-              <h2 className="headings">Residential Cleaning</h2>
+              <h2 className="headings">Landscaping/ Beautification</h2>
               <p className="paragraph">
-                I am a full-stack web developer with strong knowledge of
-                MongoDB, node.js, Javascript and front end technologies such
-                react, jquery, Ejs and counting.Graduated University with degree
-                in science of information technology. Strong mathematics and
-                programming background.
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Asperiores provident fuga enim, aperiam, ipsam eos tempora
+                veniam, aliquid repellat fugit ullam? Fugit atque, dolore magnam
+                repellat adipisci et corrupti mollitia?
               </p>
             </div>
           </div>
@@ -134,13 +127,12 @@ export default function Services() {
               <i className="icon fa-solid fa-pump-soap"></i>
             </div>
             <div className="containerHeading">
-              <h2 className="headings">Residential Cleaning</h2>
+              <h2 className="headings">Waste Management</h2>
               <p className="paragraph">
-                I am a full-stack web developer with strong knowledge of
-                MongoDB, node.js, Javascript and front end technologies such
-                react, jquery, Ejs and counting.Graduated University with degree
-                in science of information technology. Strong mathematics and
-                programming background.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel
+                cum laboriosam nemo ut veniam! Dignissimos, fugit possimus id
+                expedita quaerat voluptatibus hic illo blanditiis minima eos
+                quasi maiores, praesentium quibusdam?
               </p>
             </div>
           </div>
@@ -149,13 +141,12 @@ export default function Services() {
               <i className="icon fa-solid fa-pump-soap"></i>
             </div>
             <div className="containerHeading">
-              <h2 className="headings">Residential Cleaning</h2>
+              <h2 className="headings">Facility Management</h2>
               <p className="paragraph">
-                I am a full-stack web developer with strong knowledge of
-                MongoDB, node.js, Javascript and front end technologies such
-                react, jquery, Ejs and counting.Graduated University with degree
-                in science of information technology. Strong mathematics and
-                programming background.
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex,
+                soluta doloremque voluptas et incidunt dicta esse expedita?
+                Doloribus nesciunt incidunt reprehenderit! Sed nesciunt
+                assumenda hic! Quisquam soluta in iusto tenetur.
               </p>
             </div>
           </div>
@@ -164,13 +155,12 @@ export default function Services() {
               <i className="icon fa-solid fa-pump-soap"></i>
             </div>
             <div className="containerHeading">
-              <h2 className="headings">Residential Cleaning</h2>
+              <h2 className="headings">Gutter Cleaning</h2>
               <p className="paragraph">
-                I am a full-stack web developer with strong knowledge of
-                MongoDB, node.js, Javascript and front end technologies such
-                react, jquery, Ejs and counting.Graduated University with degree
-                in science of information technology. Strong mathematics and
-                programming background.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque,
+                enim odit eius laboriosam nesciunt asperiores molestiae adipisci
+                ducimus error officia. Maxime placeat delectus animi libero modi
+                quod. Nihil, architecto nostrum?
               </p>
             </div>
           </div>
@@ -179,13 +169,12 @@ export default function Services() {
               <i className="icon fa-solid fa-pump-soap"></i>
             </div>
             <div className="containerHeading">
-              <h2 className="headings">Residential Cleaning</h2>
+              <h2 className="headings">Swimming Pool Cleaning</h2>
               <p className="paragraph">
-                I am a full-stack web developer with strong knowledge of
-                MongoDB, node.js, Javascript and front end technologies such
-                react, jquery, Ejs and counting.Graduated University with degree
-                in science of information technology. Strong mathematics and
-                programming background.
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Placeat, aliquam voluptates blanditiis asperiores possimus harum
+                est totam obcaecati aut ex! Ea ducimus consequatur autem.
+                Perspiciatis, aliquam animi. Quo, quaerat odio!
               </p>
             </div>
           </div>
@@ -194,13 +183,12 @@ export default function Services() {
               <i className="icon fa-solid fa-pump-soap"></i>
             </div>
             <div className="containerHeading">
-              <h2 className="headings">Residential Cleaning</h2>
+              <h2 className="headings">Gardening</h2>
               <p className="paragraph">
-                I am a full-stack web developer with strong knowledge of
-                MongoDB, node.js, Javascript and front end technologies such
-                react, jquery, Ejs and counting.Graduated University with degree
-                in science of information technology. Strong mathematics and
-                programming background.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+                praesentium explicabo nobis, eius vel illum deleniti
+                repudiandae, veritatis laborum, magni ex! Earum saepe excepturi
+                molestiae hic provident atque eaque ipsa!
               </p>
             </div>
           </div>
@@ -209,13 +197,12 @@ export default function Services() {
               <i className="icon fa-solid fa-pump-soap"></i>
             </div>
             <div className="containerHeading">
-              <h2 className="headings">Residential Cleaning</h2>
+              <h2 className="headings">Decluttering</h2>
               <p className="paragraph">
-                I am a full-stack web developer with strong knowledge of
-                MongoDB, node.js, Javascript and front end technologies such
-                react, jquery, Ejs and counting.Graduated University with degree
-                in science of information technology. Strong mathematics and
-                programming background.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Nesciunt unde voluptas vero fuga, deleniti consequuntur sit
+                iusto sequi molestiae exercitationem velit explicabo ea fugit
+                quam doloremque est veniam pariatur? Sunt.
               </p>
             </div>
           </div>
@@ -224,13 +211,12 @@ export default function Services() {
               <i className="icon fa-solid fa-pump-soap"></i>
             </div>
             <div className="containerHeading">
-              <h2 className="headings">Residential Cleaning</h2>
+              <h2 className="headings">Painting Services</h2>
               <p className="paragraph">
-                I am a full-stack web developer with strong knowledge of
-                MongoDB, node.js, Javascript and front end technologies such
-                react, jquery, Ejs and counting.Graduated University with degree
-                in science of information technology. Strong mathematics and
-                programming background.
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos
+                tenetur alias explicabo quisquam, minima blanditiis fugiat quam
+                asperiores, laborum expedita vero magni sunt minus
+                exercitationem placeat. Corporis modi iste dignissimos.
               </p>
             </div>
           </div>
@@ -239,13 +225,12 @@ export default function Services() {
               <i className="icon fa-solid fa-pump-soap"></i>
             </div>
             <div className="containerHeading">
-              <h2 className="headings">Residential Cleaning</h2>
+              <h2 className="headings">Property Management</h2>
               <p className="paragraph">
-                I am a full-stack web developer with strong knowledge of
-                MongoDB, node.js, Javascript and front end technologies such
-                react, jquery, Ejs and counting.Graduated University with degree
-                in science of information technology. Strong mathematics and
-                programming background.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+                cupiditate dolorem quos maiores blanditiis temporibus facilis
+                libero esse itaque dolor obcaecati molestiae id at
+                necessitatibus iste, ad nostrum sit inventore!
               </p>
             </div>
           </div>
@@ -254,13 +239,12 @@ export default function Services() {
               <i className="icon fa-solid fa-pump-soap"></i>
             </div>
             <div className="containerHeading">
-              <h2 className="headings">Residential Cleaning</h2>
+              <h2 className="headings">Tree Planting</h2>
               <p className="paragraph">
-                I am a full-stack web developer with strong knowledge of
-                MongoDB, node.js, Javascript and front end technologies such
-                react, jquery, Ejs and counting.Graduated University with degree
-                in science of information technology. Strong mathematics and
-                programming background.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero,
+                laudantium. Vitae distinctio laboriosam eius sapiente architecto
+                debitis voluptates vero et! Tempora architecto nobis, ducimus
+                quod quibusdam optio voluptate distinctio! Impedit?
               </p>
             </div>
           </div>
