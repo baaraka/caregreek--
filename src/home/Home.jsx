f87b7d992712328290951d4f33b6d53accedf7d2
barakaganai <barakaganai01@gmail.com>
2022-12-06 20:13:23 +0300

    add div

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -5,25 +5,27 @@ export default function Home() {
     <div className="home">
       <div className="homeImg">
         <img
-          src="https://burst.shopifycdn.com/photos/holding-bucket-of-cleaning-supplies.jpg?width=1200&format=pjpg&exif=1&iptc=1"
+          src="https://img.freepik.com/premium-photo/woman-wearing-rubber-gloves-cleaning-kitchen-cabinets-surface-cleaning-concept_359031-20790.jpg?w=2000"
           alt=""
           className="homeImgItem"
         />
       </div>
-      <div className="homeTitle">
-        <h1 className="homeTitleItem">Our Services</h1>
-      </div>
-      <div className="homeList">
-        <ul className="homeListItem">
-          <li>............</li>
-          <li>............</li>
-          <li>............</li>
-          <li>............</li>
-          <li>............</li>
-          <li>............</li>
-          <li>............</li>
-          <li>............</li>
-        </ul>
+      <div className="homeTitleTitle">
+        <div className="homeTitle">
+          <h1 className="homeTitleItem">Our Services</h1>
+        </div>
+        <div className="homeList">
+          <ul className="homeListItem">
+            <li>............</li>
+            <li>............</li>
+            <li>............</li>
+            <li>............</li>
+            <li>............</li>
+            <li>............</li>
+            <li>............</li>
+            <li>............</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
